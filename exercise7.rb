f7ee148a52432f052510d5001fa7f2ea0416cfba
Mike Cooper <shivagogo@gmail.com>
2015-10-28 17:30:50 -0400

    Add cohort

--- exercise7.rb
+++ exercise7.rb
@@ -1,5 +1,7 @@
 students = { :cohort1 => 34, :cohort2 => 42, :cohort3 => 22 }
+students[:cohort4] = "43"
 students.each do |key, value|
 	puts "#{key}: #{value}"
 end
+#students[cohort4] = "43"
 #puts students
