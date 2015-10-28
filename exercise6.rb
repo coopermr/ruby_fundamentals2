38cf276476770a867218c9b21952401dbc314194
Mike Cooper <shivagogo@gmail.com>
2015-10-28 16:59:58 -0400

    Query bananas

--- exercise6.rb
+++ exercise6.rb
@@ -4,4 +4,10 @@ puts grocery_list
 grocery_list.map! {|x| "*" + x }
 puts grocery_list
 puts grocery_list.size
-
+#include?
+grocery_list.include?("bananas")
+if true
+  puts "You need to pick up bananas"
+else false
+  puts "You don't need to pick up bananas today"
+end
