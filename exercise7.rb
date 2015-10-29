0836a361e462d081c237c7b14c22a166689626f0
Mike Cooper <shivagogo@gmail.com>
2015-10-29 09:32:10 -0400

    Submitting

--- exercise7.rb
+++ exercise7.rb
@@ -1,10 +1,17 @@
+
+
 students = { :cohort1 => 34, :cohort2 => 42, :cohort3 => 22 }
+
 students[:cohort4] = "43"
+#remove
+
+students.each {|key, value| students[key] = value * 1.05 }
+
 students.each do |key, value|
 	puts "#{key}: #{value}"
 end
+
 students.each do |key, value|
   puts "#{key}"
 end
-# students[cohort4] = "43"
-# puts students
+#puts students
